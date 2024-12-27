4503ffde661143fa6dccc03eb0b4a46c361319d1
seouldavid <seouldavid98@gmail.com>
2024-12-27 14:56:12 +0900

    .

---
+++ src/main/java/com/saintkream/server/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.saintkream.server.config;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+
+        // 허용할 Origin 설정
+        corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        // 허용할 http 메서드 설정
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        // 허용할 헤더 설정
+        corsConfig.setAllowedHeaders(Arrays.asList("*"));
+        // 인증정보 허용
+        corsConfig.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return source;
+    }
+
+}
