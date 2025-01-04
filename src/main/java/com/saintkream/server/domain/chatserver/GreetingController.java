a17694309a349838c1be71c816a95c9ce060ea98
seouldavid <seouldavid98@gmail.com>
2025-01-04 11:54:50 +0900

    .

--- src/main/java/com/saintkream/server/domain/chatserver/GreetingController.java
+++ src/main/java/com/saintkream/server/domain/chatserver/GreetingController.java
@@ -1,5 +1,6 @@
 package com.saintkream.server.domain.chatserver;
 
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -8,17 +9,19 @@ import org.springframework.web.util.HtmlUtils;
 
 @Controller
 public class GreetingController {
-
-  @MessageMapping("/hello")
-  @SendTo("/topic/greetings")
-  public Greeting greeting(HelloMessage message) throws Exception {
+  //여기로 올때
+  @MessageMapping("/chat/{roomId}")
+  //다른데로 보낼때
+  @SendTo("/topic/chat/{roomId}")
+  public Greeting greeting(@DestinationVariable("roomId") String roomId, HelloMessage message) throws Exception {
+    System.out.println("roomId :"+roomId);
     System.out.println("Received Name: " + message.getName());
     System.out.println("Received Message: " + message.getMessage());
     return new Greeting(message.getName(), message.getMessage()); // name과 message를 개별적으로 반환
   }
 
-  @GetMapping("/chat")
-  public String redirectStatic() {
-    return "forward:/chat/index.html";
-  }
+  // @GetMapping("/chat")
+  // public String redirectStatic() {
+  //   return "forward:/chat/index.html";
+  // }
 }
