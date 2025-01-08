54175f97ae9bc04aab79a249ed5d0b580bb2d33b
JEONYOUNGBIN <wjsdudqls0931@naver.com>
2025-01-08 19:20:14 +0900

    관심 상품까지 완료

--- src/main/java/com/saintkream/server/domain/wishlist/service/WishListService.java
+++ src/main/java/com/saintkream/server/domain/wishlist/service/WishListService.java
@@ -2,6 +2,8 @@ package com.saintkream.server.domain.wishlist.service;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.saintkream.server.domain.wishlist.vo.WishListVO;
 
 public interface WishListService {
@@ -12,7 +14,7 @@ public interface WishListService {
     List<WishListVO> getWishList(String member_id);
 
     // 찜 지우기기
-    void deleteWishListItem(String id);
+    void deleteWishListItem(@Param("member_id") String member_id, @Param("pwr_id") String pwr_id);
 
     boolean isWishListed(String member_id, String pwr_id); // 찜 상태 확인 메서드
 
