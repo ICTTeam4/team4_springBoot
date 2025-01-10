852ef370e4b35a4b8f3d903db6a00150ce8aa846
JEONYOUNGBIN <wjsdudqls0931@naver.com>
2025-01-10 15:02:03 +0900

    신고기능 수정 했음

--- src/main/java/com/saintkream/server/domain/report/vo/ReportVO.java
+++ src/main/java/com/saintkream/server/domain/report/vo/ReportVO.java
@@ -6,8 +6,9 @@ import lombok.Data;
 
 @Data
 public class ReportVO {
+    private String report_id;
     private String member_id;         // 신고자 ID
-    private String pwr_id;           // 신고 대상 게시물 ID
+    private String board_id;           // 신고 대상 게시물 ID
     private String report_reason;  // 신고 사유
-    private String reported_at;
+    // private String created_at;
 }
