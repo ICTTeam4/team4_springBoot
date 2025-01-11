4d75df5ed90069f14f1c96f32cb141951b29fccf
Hayoon <dbs9830@gmail.com>
2025-01-11 15:08:30 +0900

    채팅완료

--- src/main/java/com/saintkream/server/config/SecurityConfig.java
+++ src/main/java/com/saintkream/server/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
                         .requestMatchers("/oauth2/**").permitAll() // URL 경로
                         .requestMatchers("/gs-guide-websocket/**", "/chat/**").permitAll()
                         // 특정 URL에 인증없이 허용
-                        .requestMatchers("/members/**","/api/**","/static/**","/images/**")
+                        .requestMatchers("/members/**","/api/**","/static/**","/images/**","/chatimages/**")
                         .permitAll()
                         .anyRequest().authenticated())
 
